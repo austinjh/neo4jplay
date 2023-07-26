4314fbc652500e1d24fcd2eaeeec1677c8988c7a
Austin Holdsworth <16243622+austinjh@users.noreply.github.com>
2023-07-26 01:04:39 +0100

    Notes

--- aks/createnamespace.sh
+++ aks/createnamespace.sh
@@ -6,4 +6,6 @@ kubectl config set-context --current --namespace=neo4jns
 
 helm install my-neo4j-release neo4j/neo4j --namespace neo4jns -f values.yaml
 
-Connect at # http://20.26.39.16:7474/browser/
+# Connect at http://20.26.39.16:7474/browser/
+# https://data-importer.neo4j.io/connection/connect
+# https://graphacademy.neo4j.com/courses/importing-data/
